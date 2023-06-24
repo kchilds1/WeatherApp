81a8893d4d5a855329ac6a66d7d621bd5cf41082
kchilds1 <kenneth.childs20@gmail.com>
2023-06-24 14:35:00 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useCallback } from 'react';
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import './App.css';
+import particlesOptions from "./particles.json";
+import Weather from './components/Weather';
+
+function App() {
+    const particlesInit = useCallback(main => {
+        loadFull(main);
+    }, [])
+
+    return (
+        <div className="App">
+            <Particles options={particlesOptions} init={particlesInit}/>
+            <Weather />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Weather.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState, useRef } from 'react';
+import ArcText from 'arc-text';
+import DayCard from './DayCard';
+
+const weatherApiKey = 'd8d5bfc23a1b4fd285c40136230106';
+
+
+
+  
+const Weather = () => {
+	const [zipCode, setZipCode] = useState('');
+	const [forecastData, setForecastData] = useState([]);
+	const url = `http://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${zipCode}&days=7&aqi=no&alerts=no`;
+
+	const getForecast = async () => {
+		const res = await fetch(url);
+		const { forecast } = await res.json();
+		console.log(forecast.forecastday);
+		setForecastData(forecast.forecastday);
+	};
+
+	useEffect(() => {
+		if(zipCode.length === 5){
+		getForecast();
+		}
+	}, [zipCode]);
+
+	const h1Ref = useRef(null);
+
+	useEffect(() => {
+		if (h1Ref.current) {
+		  const arcText = new ArcText(h1Ref.current);
+		  arcText.arc(150);
+		}
+	  }, []);
+    //CREATED THIS VARIABLE BECAUSE IT IS EASIER TO READ
+    const inputStyle = {
+	   textAlign:"center",
+	   borderRadius:"10px",
+	   cursor:"pointer",
+	};
+<script src="arc-text.js"></script>
+	return (
+		<>
+          {/* added another div in order to adjust the margin top */}
+          <div style={{}}>
+			<label htmlFor="zipCode" style={{color: "lightblue", display:"inline-block", width:"fit-content"}}>
+				<h1 ref={h1Ref}>Zip Code</h1>
+			</label>
+            <div>
+			<input
+				name="zipCode"
+				type="number"
+				placeholder="Enter your Zip Code"
+				value={zipCode}
+				onChange={e => setZipCode(e.target.value)}
+                // styled the input box's border radius and center the text
+				style={inputStyle}
+				
+			/>
+            </div>
+			{forecastData.map(({ date, day: { condition, maxtemp_f } }) => (
+				<DayCard
+					date={date}
+					conditionImageUrl={condition.icon}
+					condition={condition.text}
+					highTemp={maxtemp_f}
+				/>
+			))}
+          </div>
+		</>
+	);
+};
+
+export default Weather;
